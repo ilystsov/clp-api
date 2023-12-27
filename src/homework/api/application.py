d7b17bd4ac939a1091e8c8d8aba3709ddb9be19b
Alexandr Ovsyannikov <alexov2004@gmail.com>
2023-12-27 23:53:49 +0400

    basic api route structure

--- src/homework/api/application.py
+++ src/homework/api/application.py
@@ -1,3 +1,25 @@
 from fastapi import APIRouter
+import src.homework.api.contracts as contracts
 
 router = APIRouter()
+
+
+@router.post("/")
+def create_application(
+    creation_request: contracts.CreateApplicationRequest,
+) -> contracts.CreateApplicationResponse:
+    ...
+
+
+@router.put("/")
+def modify_application(
+    update_request: contracts.UpdateApplicationRequest,
+) -> contracts.ResponseStatus:
+    ...
+
+
+@router.delete("/")
+def delete_application(
+    delete_request: contracts.DeleteApplicationRequest,
+) -> contracts.ResponseStatus:
+    ...
