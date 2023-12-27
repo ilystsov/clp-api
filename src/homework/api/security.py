acd5e96c7b79f7029d5c08294ce4062af408fda8
Alexandr Ovsyannikov <alexov2004@gmail.com>
2023-12-28 02:54:03 +0400

    security tools added and tested

--- src/homework/api/security.py
+++ src/homework/api/security.py
@@ -1,6 +1,9 @@
+import binascii
 import typing
 import enum
 import jwt
+import base64
+import json
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 from src.homework.api.contracts import AccessLevel
@@ -18,6 +21,14 @@ BroadenAccessLevel = typing.Union[AccessLevel, MasterAccessLevel]
 def issue_token(
     app_id: str, access_level: BroadenAccessLevel, secret: str
 ) -> str:
+    """
+    Generate a token signed by secret.
+
+    :param app_id: str
+    :param access_level: BroadenAccessLevel
+    :param secret: str
+    :return: str
+    """
     return jwt.encode(
         {"app_id": app_id, "access_level": access_level.value},
         secret,
@@ -29,8 +40,57 @@ def verify_token(token: str, supposed_secret: str) -> typing.Dict[str, str]:
     return jwt.decode(token, supposed_secret, algorithms="HS256")
 
 
-def get_secret_by_app_id(app_id: str, get_url_func: typing.Callable[[], str]) -> None | Application:
+def get_app_by_id(
+    app_id: str, get_url_func: typing.Callable[[], str]
+) -> None | Application:
+    """
+    Get an application from the database by its id.
+
+    If the application with given id does not exist, function returns None.
+    :param app_id: str
+    :param get_url_func: Callable[[], str] - a function to generate connection string
+    :return: None | Application
+    """
     engine = create_engine(get_url_func())
     with Session(engine) as session:
-        app = session.scalar(select(Application).where(Application.app_id == app_id))
+        app = session.scalar(
+            select(Application).where(Application.app_id == app_id)
+        )
         return app
+
+
+def decode_segment(segment: str) -> None | typing.Dict[str, str]:
+    """
+    Decode a given segment of the JWT.
+
+    :param segment: str
+    :return: decoded JWT - dict
+    """
+    try:
+        binary_decoded = base64.b64decode(segment + "==")
+        jsonned_data = json.loads(binary_decoded.decode())
+    except (binascii.Error, json.JSONDecodeError):
+        return None
+    return jsonned_data
+
+
+def token_has_access(token: str, access_level: BroadenAccessLevel) -> bool:
+    """
+    Verify that the token has required access level.
+
+    The function is expected to be used for the FastApi header check.
+    :param token: str
+    :param access_level: BroadenAccessLevel
+    :return: bool
+    """
+    payload = decode_segment(token.split(".")[1])
+    if payload is None or payload.get("app_id") is None:
+        return False
+    app = get_app_by_id(payload.get("app_id"), get_postgres_db_url)
+    if app is None:
+        return False
+    try:
+        decoded_payload = verify_token(token, app.secret)
+    except jwt.PyJWTError:
+        return False
+    return decoded_payload["access_level"] == access_level.value
--- tests/test_security.py
+++ tests/test_security.py
@@ -1,5 +1,8 @@
+from unittest.mock import patch
 import pytest
 import src.homework.api.security as security
+from src.homework.api.contracts import AccessLevel
+from src.homework.db.models import Application
 
 
 @pytest.fixture(name="token_data")
@@ -11,17 +14,49 @@ def get_data_and_precalculated_jwt():
         },
         "secret",
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
-        "eyJhcHBfaWQiOiJ0ZXN0dGVzdHRlc3R0ZXN0dGVzdHRlc3R0ZXN0dGVzdHRlc3QiLCJhY2Nlc3NfbGV2ZWwiOiJNYXN0ZXJBcHAifQ."
+        "eyJhcHBfaWQiOiJ0ZXN0dGVzdHRlc3R0ZXN0dGVzdH"
+        "Rlc3R0ZXN0dGVzdHRlc3QiLCJhY2Nlc3NfbGV2ZWwiOiJNYXN0ZXJBcHAifQ."
         "h2gTjMWk0-5LlhWDGGUtTCGMIt-x8SRiNrCbdz5a55c",
     )
 
 
 def test_issue_token(token_data):
     data, secret, result = token_data
-    assert security.issue_token(data["app_id"],
-                                security.MasterAccessLevel(data["access_level"]), secret) == result
+    assert (
+        security.issue_token(
+            data["app_id"],
+            security.MasterAccessLevel(data["access_level"]),
+            secret,
+        )
+        == result
+    )
 
 
 def test_decode_token(token_data):
     data, secret, token = token_data
     assert security.verify_token(token, secret) == data
+
+
+def test_decode_segment(token_data):
+    data, _, token = token_data
+    assert security.decode_segment(token.split(".")[1]) == data
+
+
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        (security.MasterAccessLevel("MasterApp"), "secret", True),
+        (AccessLevel("Can_Read"), "secret", False),
+        (AccessLevel("Can_Modify_Orders"), "secret", False),
+        (security.MasterAccessLevel("MasterApp"), "nesecret", False),
+    ],
+)
+def test_token_has_access(token_data, test_case):
+    _, __, token = token_data
+    with patch(
+        "src.homework.api.security.get_app_by_id",
+        lambda x, y: Application(
+            app_id="test" * 9, app_name="test", secret=test_case[1]
+        ),
+    ):
+        assert security.token_has_access(token, test_case[0]) == test_case[2]
