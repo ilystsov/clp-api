54525694cadebfca3a351c622504c1caa70ad73f
Alexandr Ovsyannikov <alexov2004@gmail.com>
2023-12-28 16:50:14 +0400

    suggested database solution

--- src/homework/db/crud.py
+++ src/homework/db/crud.py
@@ -4,20 +4,17 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 
 from src.homework.db.models import Application
+from src.homework.db.engine import engine
 
 
-def get_app_by_id(
-    app_id: str, get_url_func: typing.Callable[[], str]
-) -> None | Application:
+def get_app_by_id(app_id: str) -> None | Application:
     """
     Get an application from the database by its id.
 
     If the application with given id does not exist, function returns None.
     :param app_id: str
-    :param get_url_func: a function to generate connection string
     :return: None | Application
     """
-    engine = create_engine(get_url_func())
     with Session(engine) as session:
         app = session.scalar(
             select(Application).where(Application.app_id == app_id)
