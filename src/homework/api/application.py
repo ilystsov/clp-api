f57033fb3c0f108884956d55d136b678db40333f
Alexandr Ovsyannikov <alexov2004@gmail.com>
2023-12-28 20:30:57 +0400

    create_application endpoint finished

--- src/homework/api/application.py
+++ src/homework/api/application.py
@@ -1,6 +1,19 @@
+from uuid import uuid4
+from secrets import token_hex
 import fastapi
-import src.homework.api.contracts as contracts
-from src.homework.api.security import ValidateHeader, MasterAccessLevel
+from src.homework.api.contracts import (
+    CreateApplicationRequest,
+    UpdateApplicationRequest,
+    DeleteApplicationRequest,
+    ResponseStatus,
+    CreateApplicationResponse,
+)
+from src.homework.api.security import (
+    ValidateHeader,
+    MasterAccessLevel,
+    issue_token,
+)
+import src.homework.db.crud as crud
 
 router = fastapi.APIRouter(
     dependencies=[
@@ -11,20 +24,46 @@ router = fastapi.APIRouter(
 
 @router.post("/application")
 def create_application(
-    creation_request: contracts.CreateApplicationRequest,
-) -> contracts.CreateApplicationResponse:
-    ...
+    creation_request: CreateApplicationRequest,
+) -> CreateApplicationResponse | ResponseStatus:
+    """
+    Create a new application using MasterApp.
+
+    Endpoint takes a CreateApplicationRequest parameter
+    Endpoint returns CreateApplicationResponse if
+    application was created succesfully, and
+    ResponseStatus otherise.
+    """
+
+    app_id, secret = str(uuid4()), token_hex(32)
+
+    if not crud.create_application(
+        app_id=app_id,
+        app_name=creation_request.application_name,
+        secret=secret,
+    ):
+        return ResponseStatus(success=False)
+
+    return CreateApplicationResponse(
+        success=True,
+        app_id=app_id,
+        token=issue_token(
+            app_id=app_id,
+            access_level=creation_request.access_level,
+            secret=secret,
+        ),
+    )
 
 
 @router.put("/application")
 def modify_application(
-    update_request: contracts.UpdateApplicationRequest,
-) -> contracts.ResponseStatus:
+    update_request: UpdateApplicationRequest,
+) -> ResponseStatus:
     ...
 
 
 @router.delete("/application")
 def delete_application(
-    delete_request: contracts.DeleteApplicationRequest,
-) -> contracts.ResponseStatus:
+    delete_request: DeleteApplicationRequest,
+) -> ResponseStatus:
     ...
--- src/homework/db/crud.py
+++ src/homework/db/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-
+import sqlalchemy.exc as exc
 from src.homework.db.models import Application
 from src.homework.db.engine import engine
 
@@ -18,3 +18,18 @@ def get_app_by_id(app_id: str) -> None | Application:
             select(Application).where(Application.app_id == app_id)
         )
         return app
+
+
+def create_application(app_id: str, app_name: str, secret: str) -> bool:
+    try:
+        new_application = Application(
+            app_id=app_id,
+            app_name=app_name,
+            secret=secret,
+        )
+        with Session(engine) as session:
+            session.add(new_application)
+            session.commit()
+    except (exc.SQLAlchemyError, exc.DBAPIError):
+        return False
+    return True
